448563dd07a570feba829f4659748ffad4dd5f7a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2020-10-08 16:35:53 +0530

    Update app.js

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -5,27 +5,38 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController ($scope) {
-    $scope.foodItems = ""
+    $scope.foodItems = "";
     $scope.message = "";
+    
     $scope.checkLunchItems = function() {
       $scope.foodItems = $scope.foodItems.trim();
 
       if ($scope.foodItems === "") {
         $scope.message = "Please enter data first.";
         $scope.noticeType = "danger";
-      }
-      else {
-        var $foodItemsArray = $scope.foodItems.split(",");
-        var $length = $foodItemsArray.length;
-        if ( ($length <= 3) || ($length == 4 && $foodItemsArray[3].trim() === "") ) {
-          $scope.message = "Enjoy!";
-          $scope.noticeType = "success";
-        }
-        else {
-          $scope.message = "Too much!";
-          $scope.noticeType = "warning";
-        }
+      }else {
+        var foodItemsArray = $scope.foodItems.split(",");
+        var length = foodItemsArray.length;
+        
+        for (var i = 0; i< length; i++)
+        {
+          if (foodItemsArray[i] === '' || foodItemsArray[i] === ' ')
+          {
+            $scope.message = "Empty item found";
+            $scope.noticeType = "danger";
+            break;
+          }else
+          {
+            if (length<=3){
+              $scope.message = "Enjoy!";
+              $scope.noticeType = "success";
+            }else{
+              $scope.message = "Too much!";
+              $scope.noticeType = "warning";
+            }
+          }
+        } 
       }
     };
-  }
+  };
 })();
